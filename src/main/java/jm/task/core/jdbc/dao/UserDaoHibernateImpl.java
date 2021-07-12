72a8aa38819b6c6de63cb48f59f60d8ea682e25c
Charik635 <cuber1319@gmail.com>
2021-07-12 13:19:13 +0300

    try-with-resource

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -19,67 +19,67 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createSQLQuery("CREATE TABLE IF NOT EXISTS `task`.`users` (\n" +
-                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
-                "  `name` VARCHAR(45) NOT NULL,\n" +
-                "  `lastName` VARCHAR(45) NOT NULL,\n" +
-                "  `age` INT NOT NULL,\n" +
-                "  PRIMARY KEY (`id`));;").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession();) {
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS `task`.`users` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `name` VARCHAR(45) NOT NULL,\n" +
+                    "  `lastName` VARCHAR(45) NOT NULL,\n" +
+                    "  `age` INT NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`));;").executeUpdate();
+            session.getTransaction().commit();
+
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createSQLQuery("DROP TABLE IF EXISTS `users` ").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS `users` ").executeUpdate();
+            session.getTransaction().commit();
 
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.persist(new User(name,lastName,age));
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.persist(new User(name, lastName, age));
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.delete(session.get(User.class,id));
-        session.getTransaction().commit();
-        session.close();
-
+        try (Session session = sessionFactory.getCurrentSession();) {
+            session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        CriteriaBuilder cb = session.getCriteriaBuilder();
-        CriteriaQuery<User> cq = cb.createQuery(User.class);
-        cq.from(User.class);
-        Query query = session.createQuery(cq);
-        List<User> users = query.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return users;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<User> cq = cb.createQuery(User.class);
+            cq.from(User.class);
+            Query query = session.createQuery(cq);
+            List<User> users = query.getResultList();
+            session.getTransaction().commit();
+            return users;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createSQLQuery("TRUNCATE TABLE `users`").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("TRUNCATE TABLE `users`").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
